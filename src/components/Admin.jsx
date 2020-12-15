e9abf54c16ac10c043a715f49643c9888cbf60b0
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-12-15 15:00:39 +0100

    login as admin to get a token

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -4,9 +4,9 @@ import Login from "./Login";
 import Backoffice from "./Backoffice";
 
 function Admin() {
-  const [admin, setAdmin] = useState(false);
+  const [admin, setAdmin] = useState("no");
 
-  return admin ? <Backoffice /> : <Login setAdmin={setAdmin} />;
+  return admin === "yes" ? <Backoffice /> : <Login setAdmin={setAdmin} />;
 }
 
 export default Admin;
